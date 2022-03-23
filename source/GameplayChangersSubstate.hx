b50e8d0200f2bfd8f9442933935c59ebe3a5e626
Mackery <70108834+homelikezero@users.noreply.github.com>
2022-03-22 19:55:27 -0500

    FRAGILE FUNKIN

--- source/GameplayChangersSubstate.hx
+++ source/GameplayChangersSubstate.hx
@@ -79,6 +79,9 @@ class GameplayChangersSubstate extends MusicBeatSubstate
 
 		var option:GameplayOption = new GameplayOption('Botplay', 'botplay', 'bool', false);
 		optionsArray.push(option);
+
+		var option:GameplayOption = new GameplayOption('Fragile Funkin', 'poison', 'bool', false);
+		optionsArray.push(option);
 	}
 
 	public function new()
