cebe6826cc69ec728adc64b3d907b70d376949bc
Mackery <70108834+homelikezero@users.noreply.github.com>
2022-03-23 21:35:57 -0500

    oopsie poopsie

--- source/HealthIcon.hx
+++ source/HealthIcon.hx
@@ -11,9 +11,6 @@ class HealthIcon extends FlxSprite
 	private var isOldIcon:Bool = false;
 	private var isPlayer:Bool = false;
 	private var char:String = '';
-	
-	//for poison
-	fragilefunkin = ClientPrefs.getGameplaySetting('poison', false);
 
 	public function new(char:String = 'bf', isPlayer:Bool = false)
 	{
